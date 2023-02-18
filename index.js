15ec1206bbe6aeabe9616f04bf414f06a1af08a9
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-02-18 13:47:17 -0600

    newImage, newItem, background

---
+++ index.js
@@ -0,0 +1,79 @@
+/* tile for background */
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object    
+}
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', ()=> {
+        object.remove()
+    })
+}
+
+/* background */
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+/* objects */
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+/* items */
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+// newItem('assets/shield.png', 165, 185)
+// newItem('assets/staff.png', 600, 100)
+
+/* newImage starter code */
+/*
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+*/
+/* newItem starter code */
+/*let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})*/
